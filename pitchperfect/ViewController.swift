fad9c37f48538b609b178bfc23e62abf1a12e7dc
Seth Singletary <singletaryseth8@gmail.com>
2022-03-15 12:25:21 -0400

    Initial Commit

---
+++ pitchperfect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pitchperfect
+//
+//  Created by Seth Singletary on 3/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
